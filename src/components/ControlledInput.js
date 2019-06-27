12a74916ba7f1cd3fb4c134b347605543419d0af
Hanna Engel <hengel@alumni.berklee.edu>
2019-06-26 21:56:01 -0700

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,11 @@
-// Code ControlledInput Component Here
+handleFirstNameChange = event => {
+    this.setState({
+      firstName: event.target.value
+    })
+  }
+   
+  handleLastNameChange = event => {
+    this.setState({
+      lastName: event.target.value
+    })
+  }
